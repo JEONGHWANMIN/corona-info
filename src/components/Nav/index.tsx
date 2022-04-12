2c02e8b0803955b7d9183c25bb423b9a2edf1b6a
JeongHwanMin <65848374+JEONGHWANMIN@users.noreply.github.com>
2022-04-12 23:40:39 +0900

    Nav icons add

--- src/components/Nav/index.tsx
+++ src/components/Nav/index.tsx
@@ -1,42 +1,45 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { Auth, NavBoxWrapper, NavMenu } from './Nav.styled';
+import React from "react";
+import { Link } from "react-router-dom";
+import { Auth, NavBoxWrapper, NavMenu } from "./Nav.styled";
+import { ImHome } from "react-icons/im";
 
 function index() {
-  return (
-    <>
-      <NavBoxWrapper>
-        <div>
-          <h1>Covid-Info</h1>
-        </div>
-        <NavMenu>
-          <li>
-            <h3>
-              <Link to={'/'}>Home</Link>
-            </h3>
-          </li>
-          <li>
-            <h3>
-              <Link to={'/chart'}>DashBoard</Link>
-            </h3>
-          </li>
-          <li>
-            <h3>
-              <Link to={'/comunity'}>Community</Link>
-            </h3>
-          </li>
-        </NavMenu>
-        <Auth>
-          <div className='border'></div>
-          <div>
-            <Link to={'/sign-in'}>SingIn</Link>
-            <span> / </span>
-            <Link to={'/sign-up'}>SignUp</Link>
-          </div>
-        </Auth>
-      </NavBoxWrapper>
-    </>
-  );
+	return (
+		<>
+			<NavBoxWrapper>
+				<div>
+					<h1>Covid-Info</h1>
+				</div>
+				<NavMenu>
+					<li>
+						<h3>
+							<Link to={"/"}>
+								<ImHome /> Home
+							</Link>
+						</h3>
+					</li>
+					<li>
+						<h3>
+							<Link to={"/chart"}>DashBoard</Link>
+						</h3>
+					</li>
+					<li>
+						<h3>
+							<Link to={"/comunity"}>Community</Link>
+						</h3>
+					</li>
+				</NavMenu>
+				<Auth>
+					<div className="border"></div>
+					<div>
+						<Link to={"/sign-in"}>SingIn</Link>
+						<span> / </span>
+						<Link to={"/sign-up"}>SignUp</Link>
+					</div>
+				</Auth>
+			</NavBoxWrapper>
+		</>
+	);
 }
 
 export default index;
