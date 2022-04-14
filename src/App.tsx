0910bfc13ff659164e4226353ed69e6aa2863520
JeongHwanMin <65848374+JEONGHWANMIN@users.noreply.github.com>
2022-04-14 20:07:15 +0900

    ReDeSigned

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,12 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Reset } from "styled-reset";
-import Dashboard from "./pages/Dashboard";
-import Community from "./pages/Community";
-import Home from "./pages/Home";
-import SignUp from "./pages/SignUp";
-import SignIn from "./pages/SignIn";
 
 function App() {
 	return (
 		<div className="App">
 			<Reset />
 			<BrowserRouter>
-				<Routes>
-					<Route path="/" element={<Home />} />
-					<Route path="/comunity" element={<Community />} />
-					<Route path="/dashboard" element={<Dashboard />} />
-					<Route path="/sign-up" element={<SignUp />} />
-					<Route path="/sign-in" element={<SignIn />} />
-				</Routes>
+				<Routes></Routes>
 			</BrowserRouter>
 		</div>
 	);
--- src/components/styles/MainPage.styled.ts
+++
@@ -1,5 +0,0 @@
-import styled from 'styled-components';
-
-export const MainPage = styled.div`
-  margin-left: 24rem;
-`;
